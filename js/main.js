ec6e01e47bf48b4af2cb2b5714a79b21f5eef8f0
paslo22 <paslo22@gmail.com>
2013-08-07 21:02:34 -0300

    Logo mas copado (creo)
    07/08/2013 09:02

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$('#container').empty()
 	$.get('login.html',function(data) {
+		$('#container').empty()
 		$('#container').append(data)
 		$('#log').css('margin-top',(window.innerHeight-196)/2)
 	})
