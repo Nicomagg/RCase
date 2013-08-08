05085547957e4815c46c73328c3ee29aaf926f14
paslo22 <paslo22@gmail.com>
2013-08-08 01:08:16 -0300

    08/08/13 01:00

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-	$.get('login.html',function(data) {
-		$('#container').empty()
-		$('#container').append(data)
-		$('#log').css('margin-top',(window.innerHeight-196)/2)
-	})
+	// $.get('login.html',function(data) {
+	// 	$('#container').empty()
+	// 	$('#container').append(data)
+	// })
+	load('login')
+	window.onhashchange = function() {
+		load((document.location.hash).substr(3))
+	}
 })
+
+load = function(dir) {
+	history.pushState(null,'','#!/'+dir)
+	$('a[target!=_blank]').off('click','**')
+	$.get('html/'+dir+'.html',function(data) {
+		$('#container').empty().append(data)
+	}).done(function() {
+		document.location.hash='#!/'+dir
+	})
+}
